cb5458bde715db266b212ce9c291a26856d4b1cc
Gabriel <gabrielmareia@hotmail.com>
2019-01-08 15:42:35 -0200

    fix conversation file

--- functions/utils/conversation.js
+++ functions/utils/conversation.js
@@ -1,6 +1,6 @@
 module.exports = {
-  WELCOME_APPEARANCE = "Hi nice to meet you! I'm Nat! I'm here to help you get the best deals and offers. How can I help you today?",
-  REJECTION_UNDERSTAND ="I didn't understand",
-  REJECTION_TRY_AGAIN = "I'm sorry, can you try again?",
-  REJECTION_ONLY_GOOGLE_REQUEST = "Only requests from Google Assistant are supported. Find the XXX action on Google Assistant directory!"
+  WELCOME_APPEARANCE:"Hi nice to meet you! I'm Nat! I'm here to help you get the best deals and offers. How can I help you today?",
+  REJECTION_UNDERSTAND:"I didn't understand",
+  REJECTION_TRY_AGAIN: "I'm sorry, can you try again?",
+  REJECTION_ONLY_GOOGLE_REQUEST:"Only requests from Google Assistant are supported. Find the XXX action on Google Assistant directory!",
 }
